b51812ba3b9c1742b7cd66cf576e807ae5ed3ed0
Jordan Anderson <32074387+janderson94@users.noreply.github.com>
2020-09-23 10:03:28 -0400

    Update impute.R

--- impute.R
+++ impute.R
@@ -1,26 +1,22 @@
 #!/usr/bin/env Rscript
-#SBATCH --get-user-env
 
+#Requires libraries impute and data.table
 library(data.table)
 
 all<-fread("all_methratios.txt")
 
-temp<-read.table("all_methratios_no_missing_n277.txt",header=T)
-
-all[,283:285]<-NULL
-
-colnames(all)<-colnames(temp)
-
 missing<-apply(all,1,function(x){length(which(is.na(x)))})
 
+#Want to remove sites missing in >5% of the data for the purposes of elastic net efficiency
 all2<-all[missing<14,]
 rownames(all2)<-all2$site
 all2$site<-NULL
 
-library(impute,lib.loc="/data/tunglab/jaa57/programs/R_packages")
+library(impute)
 
+#Save the imputed, missing methylation ratios
 all_imputed<-impute.knn(as.matrix(all2))$data
 
-dim(all_imputed)
+#Write out impute mratios for elastic net modeling
 write.table(all_imputed,"all_methratios_imputed_450k_n277.txt",quote=F,row.names=T,col.names=T)
 
