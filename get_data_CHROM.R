0f621eecd8b739e21fde518067fe78ca69d59f35
Jordan Anderson <32074387+janderson94@users.noreply.github.com>
2020-09-23 09:13:13 -0400

    Update get_data_CHROM.R

--- get_data_CHROM.R
+++ get_data_CHROM.R
@@ -1,6 +1,8 @@
 #!/usr/bin/env Rscript
 
+#Again this script is designed to run one chromosome (CHROM) at a time.
 #This R script further filters the sites from get_data_CHROM.sh based on mean methratio and coverage.
+#Requires data.table 
 library(data.table)
 data=fread('all_mratios_CHROMNAME_v2.txt',header=F)
 data[,V9:=paste(data[,V1],data[,V2],sep="_")]
@@ -35,11 +37,14 @@ mratios<-counts
 mratios[,2:(length(names)+1)]<-mcounts[,2:(length(names)+1)]/counts[,2:(length(names)+1)]
 
 #Calculate mean mratio, sd, and avg coverage
+#Note that the SD filter here is not stringent, and could be much more strict.
+#Depending on sample size and computational capacity, more stringent filtering could be done.
+#Alternatively, if CpG sites are low quality (e.g. low depth, low variance), they will simply
 mratios$avg=apply(mratios[,2:(length(names)+1)],1,mean,na.rm=TRUE)
 mratios$sd=apply(mratios[,2:(length(names)+1)],1,sd,na.rm=TRUE)
 mratios$depth<-apply(counts[,2:(length(names)+1)],1,mean,na.rm=TRUE)
 
-#Filter
+#Filte
 mratios2<-subset(mratios,avg<.9 & avg>.1 & sd > quantile(mratios$sd,probs=.05) & depth >5)
 mcounts2<-mcounts[site %in%mratios2$site]
 counts2<-counts[site %in%mratios2$site]
